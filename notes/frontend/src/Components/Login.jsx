cab678fc89d12332be07a49d3d22f9cfe288d637
born hyper <ROHINI SINGH>
2024-07-01 14:15:32 +0530

    signup page added

--- notes/frontend/src/Components/Login.jsx
+++ notes/frontend/src/Components/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
   <div className="modal-box">
     <form method="dialog">
       {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
     </form>
     <h3 className="font-bold text-lg">Login</h3>
      {/*Email*/}
