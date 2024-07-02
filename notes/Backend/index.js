b6d6519adfefc86fccfca22853d91bd7bc1f4a15
born hyper <ROHINI SINGH>
2024-07-02 12:54:53 +0530

    added the data to the interface from the databse

--- notes/Backend/index.js
+++ notes/Backend/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import cors from "cors";
 
 import notesRoute from "./route/notes.route.js"
 
+
 const app =express();
+app.use(cors());
 
 dotenv.config();
 const PORT=process.env.PORT||4000;
